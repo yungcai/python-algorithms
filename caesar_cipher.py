50758663d7c4378f8c124c5b0e8959c67beeb6b0
Yung Cai <yungcai09@gmail.com>
2021-12-25 00:05:17 -0500

    caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,10 @@
+def caesarCipher(string, key):
+    newLetters = []
+    newKey = key % 26
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey))
+        return ''.join(newLetters)
+
+def getNewLetter(letter, key):
+    newLetterCode = ord(letter) + key
+    return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
