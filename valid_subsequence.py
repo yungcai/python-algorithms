3c268fff55ed0387eb5bf218a231b1f57281c04e
Yung Cai <yungcai09@gmail.com>
2021-12-27 00:05:14 -0500

    remove dups

--- valid_subsequence.py
+++ valid_subsequence.py
@@ -2,7 +2,7 @@ def isValidSubsequence(arr, seq):
     arrIdx = 0
     seqIdx = 0
     while arrIdx < len(arr) and seqIdx < len(seq):
-        if arr[arrIdx] == deq[seqIdx]:
+        if arr[arrIdx] == seq[seqIdx]:
             seqIdx += 1
         arrIdx+= 1
     return seqIdx == len(seq)
